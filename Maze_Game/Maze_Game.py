a7db8c20af21e5e49b6c2673fa1aa3fb350e18f9
Yuriy Drubinskiy <ydrubins@yahoo.com>
2023-05-29 09:55:45 -0500

    First iteration of the maze game given by ChatGPT. The prompt was "can you make it so this object moves within the maze you generated in the previous few codes?"

---
+++ Maze_Game/Maze_Game.py
@@ -0,0 +1,108 @@
+import pygame
+
+# Constants
+CELL_SIZE = 20
+ROWS = 15
+COLS = 15
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+
+# Initialize Pygame
+pygame.init()
+
+# Create the window
+window_width = COLS * CELL_SIZE
+window_height = ROWS * CELL_SIZE
+window = pygame.display.set_mode((window_width, window_height))
+
+# Maze grid
+grid = [[0] * COLS for _ in range(ROWS)]
+
+# Start position
+start_row = 0
+start_col = 0
+
+# End position
+end_row = ROWS - 1
+end_col = COLS - 1
+
+# Movable object
+class MovableObject:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def move_up(self):
+        if self.row > 0 and not grid[self.row][self.col] & 1:
+            self.row -= 1
+
+    def move_down(self):
+        if self.row < ROWS - 1 and not grid[self.row + 1][self.col] & 1:
+            self.row += 1
+
+    def move_left(self):
+        if self.col > 0 and not grid[self.row][self.col - 1] & 2:
+            self.col -= 1
+
+    def move_right(self):
+        if self.col < COLS - 1 and not grid[self.row][self.col] & 2:
+            self.col += 1
+
+# Create a movable object
+movable_object = MovableObject(start_row, start_col)
+
+# Draw the maze and movable object
+def draw_maze():
+    window.fill(BLACK)
+    for row in range(ROWS):
+        for col in range(COLS):
+            x = col * CELL_SIZE
+            y = row * CELL_SIZE
+
+            if grid[row][col] & 1:  # Check if top wall is present
+                pygame.draw.line(window, WHITE, (x, y), (x + CELL_SIZE, y), 1)
+            if grid[row][col] & 2:  # Check if right wall is present
+                pygame.draw.line(
+                    window, WHITE, (x + CELL_SIZE, y), (x + CELL_SIZE, y + CELL_SIZE), 1
+                )
+            if grid[row][col] & 4:  # Check if bottom wall is present
+                pygame.draw.line(
+                    window, WHITE, (x, y + CELL_SIZE), (x + CELL_SIZE, y + CELL_SIZE), 1
+                )
+            if grid[row][col] & 8:  # Check if left wall is present
+                pygame.draw.line(window, WHITE, (x, y), (x, y + CELL_SIZE), 1)
+
+    pygame.draw.rect(window, GREEN, (start_col * CELL_SIZE, start_row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    pygame.draw.rect(window, YELLOW, (end_col * CELL_SIZE, end_row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    pygame.draw.circle(
+        window,
+        YELLOW,
+        (movable_object.col * CELL_SIZE + CELL_SIZE // 2, movable_object.row * CELL_SIZE + CELL_SIZE // 2),
+        CELL_SIZE // 2
+    )
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                movable_object.move_up()
+            elif event.key == pygame.K_DOWN:
+                movable_object.move_down()
+            elif event.key == pygame.K_LEFT:
+                movable_object.move_left()
+            elif event.key == pygame.K_RIGHT:
+                movable_object.move_right()
+
+    draw_maze()
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
